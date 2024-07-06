ecab14feb1fe6d00e4766e7a6601093c26676fff
MaryamArif7 <www.maryamarif28@gmail.com>
2024-07-06 19:47:11 +0500

    starting again

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Signup from "./pages/Signup/Signup";
 import Contact from "./pages/Contact/Contact";
+import Login from "./pages/Login/Login"
 import Footer  from "./components/Footer";
 function App() {
  return (
@@ -12,9 +13,10 @@ function App() {
   <Navbar />
 
   <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
   <Footer />
--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,7 +1,8 @@
 import express from "express";
-import {signup} from "../controllers/auth.js"
+import {signup,Login} from "../controllers/auth.js"
+
 const router=express.Router();
 //router.post("/login",login);
 router.post("/signup", signup);
-
+router.post("/login",Login)
 export default router;

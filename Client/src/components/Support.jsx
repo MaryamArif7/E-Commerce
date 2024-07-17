d1d5a19a221a9bff3c5408c684435e1c6e5e0dd5
MaryamArif7 <www.maryamarif28242@gmail.com>
2024-07-17 13:36:28 +0500

    setting up redux store

--- Client/src/components/Support.jsx
+++ Client/src/components/Support.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+
+import Delivery from "../assets/arrival/Delivery.svg"
+import money from "../assets/arrival/monery.svg";
+import Service from "../assets/arrival/Service.svg"
 
 const Support = () => {
   return (
-    <div>Support</div>
+    <div  className='mt-28 mr-36 ml-36 flex justify-between'>
+      <div>
+     <img src={Delivery} alt="Delivery on time" />
+     <h1 className='mt-2 font-bold text-lg'>Free Delivery On Time</h1>
+      <h6>Free Delivery For All Orders over $140</h6>
+ 
+      </div>
+      <div>
+     <img src={money} alt="Money Return" />
+     <h1 className='mt-2 font-bold text-lg' >Money Back Gurantee</h1>
+      <p>We Return Money Within 30 Days</p>
+ 
+      </div>
+      <div>
+     <img src={Service} alt="Customer Support" />
+     <h1  className='mt-2 font-bold text-lg'>24/7 Customer Support </h1>
+      <p>Friendly Customer Service Available All the Time</p>
+ 
+      </div>
+
+
+
+
+
+    </div>
   )
 }
 
--- Client/src/constants/index.js
+++ Client/src/constants/index.js
@@ -16,6 +16,9 @@ import shoes from "../assets/Explore/shoes.svg";
 import skin from "../assets/Explore/skin.svg";
 import toys from "../assets/Explore/toys.svg"
 import remotee from "../assets/Explore/remotee.svg";
+import facebook from "../assets/icons/facebook.svg";
+import instagram from "../assets/icons/instagram.svg";
+import twitter from "../assets/icons/twitter.svg";
 
 export const products = [
     {
@@ -126,4 +129,41 @@ export const Explores= [
     },
     
     
+];
+
+export const footerLinks = [
+    {
+        title: "Products",
+        links: [
+            { name: "Air Force 1", link: "/" },
+            { name: "Air Max 1", link: "/" },
+            { name: "Air Jordan 1", link: "/" },
+            { name: "Air Force 2", link: "/" },
+            { name: "Nike Waffle Racer", link: "/" },
+            { name: "Nike Cortez", link: "/" },
+        ],
+    },
+    {
+        title: "Help",
+        links: [
+            { name: "About us", link: "/" },
+            { name: "FAQs", link: "/" },
+            { name: "How it works", link: "/" },
+            { name: "Privacy policy", link: "/" },
+            { name: "Payment policy", link: "/" },
+        ],
+    },
+    {
+        title: "Get in touch",
+        links: [
+            { name: "customer@nike.com", link: "mailto:customer@nike.com" },
+            { name: "+92554862354", link: "tel:+92554862354" },
+        ],
+    },
+];
+
+export const socialMedia = [
+    { src: facebook, alt: "facebook logo" },
+    { src: twitter, alt: "twitter logo" },
+    { src: instagram, alt: "instagram logo" },
 ];

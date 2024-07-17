f9b1d22289af0051e93c2697a64c36d83b96f5df
MaryamArif7 <www.maryamarif28@gmail.com>
2024-07-17 13:36:28 +0500

    setting up redux store

--- Client/src/components/Arrival.jsx
+++ Client/src/components/Arrival.jsx
@@ -1,40 +1,66 @@
-import React from 'react'
+import React from "react";
 import Speaker from "../assets/arrival/Speaker.svg";
-import Spekaers from "../assets/arrival/Speakers.svg";
+import Speakers from "../assets/arrival/Speakers.svg";
 import woman from "../assets/arrival/woman.svg";
 import Perfume from "../assets/arrival/Perfume.svg";
-import rectangle from "../assets/category/rectangle.svg"
+import rectangle from "../assets/category/rectangle.svg";
 
 const Arrival = () => {
   return (
     <div className="ml-36 mr-36 mt-14">
-           <div className='flex gap-3 mb-5'>
-          <img src={rectangle} alt="Rectangle" />
-          <p className="text-red-700">Featured</p>
-        </div>
-     <div className='mb-5'>
-     <h1 className="text-black font-bold text-2xl">Browse Categories</h1>
-     
-     </div>
-    <div className="grid grid-cols-2 gap-3 ">
-      <div className=" relative w-[511px] h-[520px] bg-black row-span-2">
-        <img className="absolute  left-0 p-4 object-contain w-full h-full" src={Speaker} alt="Image 1" />
-        <div className='text-white'>
-          <h1  className='text-2xl '>     Playstation5     </h1>
-          <p>Buy These Speakers</p>
-
-        </div>
+      <div className="flex gap-2 mb-5">
+        <img src={rectangle} alt="Rectangle" />
+        <p className="text-red-700">Featured</p>
       </div>
-      <div className="h-[240px] w-[511px] bg-black">
-        <img className="object-contain w-full h-full" src={woman} alt="Image 2" />
+      <div className="mb-5">
+        <h1 className="text-black font-bold text-2xl">New Arrivals</h1>
       </div>
-      <div className="h-[240px] w-[511px] grid grid-cols-2 gap-3">
-        <img className="bg-black w-full h-full object-contain" src={Spekaers} alt="Image 3" />
-        <img className="bg-black w-full h-full object-contain" src={Perfume} alt="Image 4" />
+      <div className="grid grid-cols-2 gap-3 ">
+        <div className="relative w-[511px] h-[520px] bg-black row-span-2">
+          <img className="p-4 object-contain w-full h-full" src={Speaker} alt="Image 1" />
+          <div className="absolute bottom-28 left-5 text-white">
+            <h1 className="text-2xl">Playstation5</h1>
+            <p>Buy These Speakers</p>
+            <button className="border-none text-white text-center mt-1 underline decoration-solid">
+              Shop Now
+            </button>
+          </div>
+        </div>
+        <div className="relative h-[240px] w-[511px] bg-black">
+          <img className="object-contain w-full h-full" src={woman} alt="Image 2" />
+          <div className="absolute bottom-14 left-5 text-white">
+            <h1 className="text-2xl">Women Collection</h1>
+            <p>Featured Women Collection that <br /> give you another vibe</p>
+            <button className="border-none text-white text-center mt-1 underline decoration-solid">
+              Shop Now
+            </button>
+          </div>
+        </div>
+        <div className="relative h-[240px] w-[511px] grid grid-cols-2 gap-3">
+          <div className="relative">
+            <img className="bg-black w-full h-full object-contain" src={Speakers} alt="Image 3" />
+            <div className="absolute bottom-10 left-5 text-white">
+              <h1 className="text-2xl">Speakers</h1>
+              <p>Amazon Wireless Speakers</p>
+              <button className="border-none text-white text-center mt-1 underline decoration-solid">
+                Shop Now
+              </button>
+            </div>
+          </div>
+          <div className="relative">
+            <img className="bg-black w-full h-full object-contain" src={Perfume} alt="Image 4" />
+            <div className="absolute bottom-10 left-5 text-white">
+              <h1 className="text-2xl">Perfume</h1>
+              <p>Amazon Perfume Collection</p>
+              <button className="border-none text-white text-center mt-1 underline decoration-solid">
+                Shop Now
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Arrival
+export default Arrival;
--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import copyrightSign from "../assets/icons/copyright-sign.svg";
+import { socialMedia, footerLinks } from "../constants";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className='mt-10 bg-black p-10 pb-5 pr-28 pl-10'>
+      <div className='flex justify-between items-start gap-20 flex-wrap max-lg:flex-col'>
+        <div className='flex flex-col items-start'>
+          <p className="mt-6 text-base leading-7 font-montserrat text-gray-400 sm:max-w-sm">Get anything you want</p>
+          <div className="flex items-center gap-3 mt-8">
+            {socialMedia.map((icon, index) => (
+              <div key={index} className="flex justify-center items-center w-10 h-10 bg-white rounded-full">
+                <img src={icon.src} alt={icon.alt} className="w-6 h-6" />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-1 justify-between lg:gap-10 gap-10 flex-wrap">
+          {footerLinks.map((section, index) => (
+            <div key={index}>
+              <h4 className="text-white font-montserrat text-2xl leading-normal font-medium mb-6">{section.title}</h4>
+              <ul>
+                {section.links.map((link, idx) => (
+                  <li key={idx} className="mt-3 text-gray-400 font-montserrat text-base leading-normal hover:text-gray-300 cursor-pointer">
+                    <a href={link.url || "#"}>{link.name}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex justify-between text-gray-400 mt-5 max-sm:flex-col max-sm:items-center">
+        <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
+          <img src={copyrightSign} alt="copyright" className="w-5 h-5 rounded-full m-0" />
+          <p>Copyright. All rights reserved.</p>
+        </div>
+        <p className="font-montserrat cursor-pointer">Terms & Conditions</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;

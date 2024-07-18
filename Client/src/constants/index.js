26c97b5aa462bc490b04d3c7fef6c9d0c30f3f5b
MaryamArif7 <www.maryamarif28@gmail.com>
2024-07-18 17:05:30 +0500

    changed email

--- Client/src/constants/index.js
+++ Client/src/constants/index.js
@@ -135,12 +135,12 @@ export const footerLinks = [
     {
         title: "Products",
         links: [
-            { name: "Air Force 1", link: "/" },
-            { name: "Air Max 1", link: "/" },
-            { name: "Air Jordan 1", link: "/" },
-            { name: "Air Force 2", link: "/" },
-            { name: "Nike Waffle Racer", link: "/" },
-            { name: "Nike Cortez", link: "/" },
+            { name: "women", link: "/" },
+            { name: "MEN", link: "/" },
+            { name: "Shirts", link: "/" },
+            { name: "Tops", link: "/" },
+            { name: "Speakers", link: "/" },
+            { name: "Shoes", link: "/" },
         ],
     },
     {
@@ -156,7 +156,7 @@ export const footerLinks = [
     {
         title: "Get in touch",
         links: [
-            { name: "customer@nike.com", link: "mailto:customer@nike.com" },
+            { name: "customer@maryam.com", link: "mailto:customer@maryam.com" },
             { name: "+92554862354", link: "tel:+92554862354" },
         ],
     },

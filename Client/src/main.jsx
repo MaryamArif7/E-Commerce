dc866f6d20a538560f7369f681720cf81bd81bb2
MaryamArif7 <www.maryamarif28242@gmail.com>
2024-07-22 15:49:49 +0500

    setting up redux

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Provider } from 'react-router-dom';
+import counterStore from './store/index.js';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={counterStore}>
     <App />
+    </Provider>
+  
   </React.StrictMode>,
 )

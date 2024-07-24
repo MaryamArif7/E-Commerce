289999807b53e893344596346f88f26e91204992
MaryamArif7 <www.maryamarif28@gmail.com>
2024-07-24 10:48:39 +0500

    added:redux

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Provider } from 'react-router-dom';
-import counterStore from './store/index.js';
+import { Provider } from 'react-redux';
+import store from './store/index.js';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={counterStore}>
+    <Provider store={store}>
     <App />
     </Provider>
   
